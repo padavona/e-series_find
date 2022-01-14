922767ba439e42422f57e3a62d5ecaf99fdddb19
padavona <darth_sidious@gmx.net>
2022-01-14 07:51:59 +0100

    Add method to get e-rows of a specific value

--- erow_find.py
+++ erow_find.py
@@ -10,7 +10,7 @@ import math
 ############################
 
 # desired value
-desired = 999
+desired = 1.9e3
 
 # is desired value an upper bound?
 # False: Find value that is closest to desired value
@@ -31,7 +31,7 @@ y_stop = 1e6
 
 # set of e-rows (e12, e24, e48 and e96 supported),
 # e. g. x_e_row = {"e12, "e24"}
-y_e_row = {"e12", "e24", "e48", "e96"}
+y_e_row = {"e12"}
 
 # Select desired formula by un-commenting lambda or add own lambda
 # func = lambda x, y: (2 * x / y) + 1  # instrumentation amplifier gain
@@ -43,9 +43,7 @@ func = lambda x, y: x + y  # series resistors
 # func = lambda x, y: 1.21 * (1 + (y / x)) + (3e-6 * x)  # TPS73801 LDO output voltage
 # func = lambda x, y: y / (x + y)  # voltage divider (desired value is "output" to "input" voltage)
 # func = lambda x, y: 1.23 * (1 + x / y) + (-20e-9 * x)  # LP2954 output voltage
-# func = lambda x, y: 1 / (
-#     2 * math.pi * (350 + 2 * x) * y / 1e9
-# )  # differential analog filter on GMS (with "R2" in "nF")
+# func = lambda x, y: 1 / (2 * math.pi * (350 + 2 * x) * y)  # differential analog filter on GMS
 # func = lambda x, y: 1 / (
 #     2 * math.pi * (350 + 2 * x) * (y / 1e9 + 0.5 / 1e9)
 # )  # combined DM+CM analog filter on GMS (with "R2" in "nF" and 1 nF from each line to ground as CM part)
@@ -79,6 +77,201 @@ def get_start_decade(start: float) -> float:
         return 10 ** index
     else:
         return None
+
+
+def get_e_row(value: float) -> Generator[str, None, None]:
+    assert value > 0, "value has to be greater than 0"
+    e12 = {1.0, 1.2, 1.5, 1.8, 2.2, 2.7, 3.3, 3.9, 4.7, 5.6, 6.8, 8.2}
+    e24 = {
+        1.0,
+        1.1,
+        1.2,
+        1.3,
+        1.5,
+        1.6,
+        1.8,
+        2.0,
+        2.2,
+        2.4,
+        2.7,
+        3.0,
+        3.3,
+        3.6,
+        3.9,
+        4.3,
+        4.7,
+        5.1,
+        5.6,
+        6.2,
+        6.8,
+        7.5,
+        8.2,
+        9.1,
+    }
+    e48 = {
+        1.00,
+        1.05,
+        1.10,
+        1.15,
+        1.21,
+        1.27,
+        1.33,
+        1.40,
+        1.47,
+        1.54,
+        1.62,
+        1.69,
+        1.78,
+        1.87,
+        1.96,
+        2.05,
+        2.15,
+        2.26,
+        2.37,
+        2.49,
+        2.61,
+        2.74,
+        2.87,
+        3.01,
+        3.16,
+        3.32,
+        3.48,
+        3.65,
+        3.83,
+        4.02,
+        4.22,
+        4.42,
+        4.64,
+        4.87,
+        5.11,
+        5.36,
+        5.62,
+        5.90,
+        6.19,
+        6.49,
+        6.81,
+        7.15,
+        7.50,
+        7.87,
+        8.25,
+        8.66,
+        9.09,
+        9.53,
+    }
+    e96 = {
+        1.00,
+        1.02,
+        1.05,
+        1.07,
+        1.10,
+        1.13,
+        1.15,
+        1.18,
+        1.21,
+        1.24,
+        1.27,
+        1.30,
+        1.33,
+        1.37,
+        1.40,
+        1.43,
+        1.47,
+        1.50,
+        1.54,
+        1.58,
+        1.62,
+        1.65,
+        1.69,
+        1.74,
+        1.78,
+        1.82,
+        1.87,
+        1.91,
+        1.96,
+        2.00,
+        2.05,
+        2.10,
+        2.15,
+        2.21,
+        2.26,
+        2.32,
+        2.37,
+        2.43,
+        2.49,
+        2.55,
+        2.61,
+        2.67,
+        2.74,
+        2.80,
+        2.87,
+        2.94,
+        3.01,
+        3.09,
+        3.16,
+        3.24,
+        3.32,
+        3.40,
+        3.48,
+        3.57,
+        3.65,
+        3.74,
+        3.83,
+        3.92,
+        4.02,
+        4.12,
+        4.22,
+        4.32,
+        4.42,
+        4.53,
+        4.64,
+        4.75,
+        4.87,
+        4.99,
+        5.11,
+        5.23,
+        5.36,
+        5.49,
+        5.62,
+        5.76,
+        5.90,
+        6.04,
+        6.19,
+        6.34,
+        6.49,
+        6.65,
+        6.81,
+        6.98,
+        7.15,
+        7.32,
+        7.50,
+        7.68,
+        7.87,
+        8.06,
+        8.25,
+        8.45,
+        8.66,
+        8.87,
+        9.09,
+        9.31,
+        9.53,
+        9.76,
+    }
+
+    if value >= 10:
+        while value >= 10:
+            value /= 10
+    elif value < 1.0:
+        while value < 1.0:
+            value *= 10
+
+    if value in e12:
+        yield "E-12"
+    if value in e24:
+        yield "E-24"
+    if value in e48:
+        yield "E-48"
+    if value in e96:
+        yield "E-96"
 
 
 def get_base_values(erows: set[str]) -> set[float]:
@@ -278,7 +471,8 @@ def get_values(start: float, stop: float, base_values: set[float]) -> Generator[
     first_decade = get_start_decade(start)
     no_of_decades = get_decades(first_decade, stop)
 
-    decades = [first_decade]
+    decades = []
+    decades.append(first_decade)
     for i in range(no_of_decades):
         decades.append(decades[i] * 10)
 
